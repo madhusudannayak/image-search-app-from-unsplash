a262a3f452557ae672b2ba98fb6fb027d8a4c3d8
Madhusudan Nayak <madhusudannayak80@gmail.com>
2021-08-18 21:11:13 +0530

    update

---
+++ app/src/main/java/com/example/imagesearchapp/ImageSearchApplication.kt
@@ -0,0 +1,9 @@
+package com.example.imagesearchapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ImageSearchApplication : Application() {
+
+}
